8747d5339b420932a90c5ab63575a960b202db9f
Marc Byndas <byndasma@gmail.com>
2024-05-13 15:40:40 -0400

    graded assignment #2 completed & refactored --> all tests pass

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -1,35 +1,5 @@
 package org.launchcode.techjobs.oo;
 
 public class Location extends JobField {
-
-//    private int id;
-//    private static int nextId = 1;
-//    private String value;
-    ////////////////////////////////////////////////////////////////////////////
-//    public Location() {
-//        id = nextId;
-//        nextId++;
-//    }
     public Location(String value) { super(value); }
-
-//    public Location(String value) {
-//        this(); // initializes id by default
-//        this.value = value;
-//    }
-    ////////////////////////////////////////////////////////////////////////////
-//    @Override
-//    public boolean equals(Object o) {
-//        // objects are equal if share same id
-//        if (this == o) return true;
-//        if (!(o instanceof Location)) return false;
-//        Location location = (Location) o;
-//        return getId() == location.getId();
-//    }
-    ////////////////////////////////////////////////////////////////////////////
-//    public int getId() { return id; }
-//
-//    public String getValue() { return value; }
-//    public void setValue(String value) {
-//        this.value = value;
-//    }
 }
